7b360fc2697d08ef3e28a405bfbf9350d3572e1a
Afifa peerzade <peerzadeafifa30@gmail.com>
2026-04-07 19:58:04 +0530

    Add lyrics printing functionality with delays

---
+++ lyrics.py
@@ -0,0 +1,37 @@
+import time
+lyrics=["Raahon main mere saath chal tu",
+        "thaame mera haath cha tu",
+        "vakt jitna bhee ho hasil",
+        "sarre mere naam kar too",
+        
+]
+for line in lyrics:
+        print(line)
+        #print()
+        time.sleep(4.2)
+lyrics1=["vakt jitna bhee ho hasil",
+        "sarre mere naam kar too",
+        "ki armaan hain ye",
+        "guzarish jaan hai ye",
+       
+        ]
+for line in lyrics1:
+        print(line)
+        #print()
+        time.sleep(4.4)
+
+        lyrics3=["too mere bahon main duniya bhula de",]
+for line in lyrics3:
+        print(line)
+        #print()
+        time.sleep(9)
+
+lyrics2=[ 
+         "jo ab lamhat hai ye",
+        "bade hi khass hai ye",
+        "too mere bahon main duniya bhula de",
+        ]
+for line in lyrics2:
+        print(line)
+        #print()
+        time.sleep(4.9)
